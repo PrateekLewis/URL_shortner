1e6881429716d63d27126d8d5a032c69cb9138d2
PrateekLewis <112384262+PrateekLewis@users.noreply.github.com>
2023-07-14 14:18:53 +0530

    Add files via upload

---
+++ url shortner.py
@@ -0,0 +1,28 @@
+import requests
+
+
+def shorten_link(full_link, link_name):
+    API_KEY = '034e10c143fa968647fdd95273e60841967a0'
+    BASE_URL = 'https://cutt.ly/api/api.php'
+
+    payload = {'key' : API_KEY, 'short' : full_link, 'name' : link_name}
+    request = requests.get(BASE_URL, params=payload)
+    data = request.json()
+
+    print('')
+
+    try:
+        title = data['url']['title']
+        short_link = data['url']['shortLink']
+
+        print('Title:', title)
+        print('Link:', short_link)
+    except:
+        status = data['url']['status']
+        print('Error Status:', status)
+
+link = input('Enter a link: >> ')
+name = input('Give your link a name: >> ')
+
+shorten_link(link, name)
+
